9092c336c9e1634095241ff60b59ff898df31580
Jaimel de Oliveira Lima <jaimeloliveiralima@gmail.com>
2022-07-07 15:22:01 -0300

    Ajustes

--- code/evaluation.py
+++ code/evaluation.py
@@ -4,7 +4,7 @@ import config as cfg
 import numpy as np
 import json
 
-experiment_file_name = "{}experiment.json".format(cfg.KAGGLE_100)
+experiment_file_name = "{}ram_4_exp.json".format(cfg.KAGGLE_100)
 print(experiment_file_name)
 
 experiment_data = []
@@ -18,22 +18,24 @@ y_pred = None
 with open(experiment_file_name) as f:
    experiment_file = json.load(f)
 for item in experiment_file:
-   y_test_matrix = []
-   y_pred_matrix = []
-   ram = experiment_file[item]['ram']
-   term = experiment_file[item]['term']
-   seed = experiment_file[item]['seed']
-   y_test = experiment_file[item]['y-test']
-   y_pred = experiment_file[item]['y-pred']
-   print(ram, term, seed, len(y_test), len(y_pred))
-   acc = accuracy_score(y_test, y_pred)
-   for label in y_test:
-        y_test_matrix.append([int(i) for i in label])
-   y_test_matrix = np.array(y_test_matrix)
-   for label in y_pred:
-        y_pred_matrix.append([int(i) for i in label])
-   y_pred_matrix = np.array(y_pred_matrix)
-   hl = hamming_loss(y_test_matrix, y_pred_matrix)
-   print(ram, term, seed, acc, hl)
+     print(item)
+     y_test_matrix = []
+     y_pred_matrix = []
+     ram = experiment_file[item]['ram']
+     term = experiment_file[item]['term']
+     seed = experiment_file[item]['seed']
+     y_test = experiment_file[item]['y-test']
+     y_pred = experiment_file[item]['y-pred']
+     print(ram, term, seed, len(y_test), len(y_pred))
+     acc = accuracy_score(y_test, y_pred)
+     for label in y_test:
+          y_test_matrix.append([int(i) for i in label])
+     y_test_matrix = np.array(y_test_matrix)
+     for label in y_pred:
+          y_pred_matrix.append([int(i) for i in label])
+     y_pred_matrix = np.array(y_pred_matrix)
+     hl = hamming_loss(y_test_matrix, y_pred_matrix)
+     print(ram, term, seed, acc, hl)
+     print()
 
 
--- code/experiment.py
+++ code/experiment.py
@@ -14,65 +14,48 @@ import re
 from threading import Thread
 
 class Experiment(Thread):
-   def __init__(self):
+   def __init__(self, ram):
       Thread.__init__(self)
       self.__today = date.today()
       self.__experiment = {}
+      self.__ram = ram
+
+   def experiment(self, seed, ram, term_size):
+      print("Term size: {}".format(term_size))
+      fname_df_features = "{}{}_documents.csv".format(cfg.KAGGLE_100, term_size)
+      print(fname_df_features)
+      df_features = pd.read_csv(fname_df_features)
+      X = df_features.to_numpy()
+      print("Documents matrix shape: {}".format(X.shape))
+      file_labels = "{}labels.json".format(cfg.KAGGLE_100)
+      print(file_labels)
+      y = None
+      with open(file_labels) as f:
+         data = json.load(f)
+      for item in data:
+         y = np.array(data[item])
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state = seed)
+      clf = Classifier()
+      y_pred = clf.wisard(X_train, y_train, X_test, ram)
+      clf = Classifier()
+      y_train_pred = clf.wisard(X_train, y_train, X_train, ram)
+      index = "e-{}-{}-{}-{}".format(ram, term_size, seed, self.__today)
+      self.__experiment[index] = {"ram": ram, "term": term_size, "seed": seed, "y-test": list(y_test),"y-pred": list(y_pred), "y-train": list(y_train),"y-train-pred": list(y_train_pred)}
 
    def run(self):
-            
-
-
-
-for seed in range(0, cfg.SEEDS):
-   for ram in range(cfg.MIN_RAM_SIZE, cfg.MAX_RAM_SIZE+1):
+      print("Starting thread...")
       for term_size in range(cfg.MIN_TERM_SIZE, cfg.MAX_TERM_SIZE+1):
-         print("Term size: {}".format(term_size))
-         fname_df_features = "{}{}_documents.csv".format(cfg.KAGGLE_1000, term_size)
-         print(fname_df_features)
-         df_features = pd.read_csv(fname_df_features)
-         X = df_features.to_numpy()
-         print("Documents matrix shape: {}".format(X.shape))
-         file_labels = "{}labels.json".format(cfg.KAGGLE_1000)
-         print(file_labels)
-         y = None
-         with open(file_labels) as f:
-            data = json.load(f)
-         for item in data:
-            y = np.array(data[item])
-         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state = seed)
-         clf = Classifier()
-         y_pred = clf.wisard(X_train, y_train, X_test, ram)
-         clf = Classifier()
-         y_train_pred = clf.wisard(X_train, y_train, X_train, ram)
-         index = "e-{}-{}-{}-{}".format(ram, term_size, seed, today)
-         experiment[index] = {"ram": ram, "term": term_size, "seed": seed, "y-test": list(y_test),"y-pred": list(y_pred), "y-train": list(y_train),"y-train-pred": list(y_train_pred)}
+         for seed in range(0, cfg.SEEDS):
+            print("Starting ram training and test experiment")
+            self.experiment(seed, self.__ram, term_size)
+      
+      experiment_file_name = "{}ram_{}_exp.json".format(cfg.KAGGLE_100, ram)
+      print("Saving label file {}".format(experiment_file_name))
+      with open(experiment_file_name, 'w') as f:
+         json.dump(self.__experiment, f)
+      print("Saving label file {}".format(experiment_file_name))
 
-experiment_file_name = "{}experiment.json".format(cfg.KAGGLE_100)
-print("Saving label file {}".format(experiment_file_name))
-with open(experiment_file_name, 'w') as f:
-   json.dump(experiment, f)
-print("Saving label file {}".format(experiment_file_name))
-
-
-# df_features = pd.read_csv(cfg.KAGGLE_100 3_features.csv)
-# file_labels = "/home/jolima/Documentos/multilabel-classification/dataset/binaries/kaggle_100/labels.json"
-# X = df_features.to_numpy()
-# y = None
-# with open(file_labels) as f:
-#    data = json.load(f)
-# for item in data:
-#    y = np.array(data[item])
-# print(type(y))
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-
-# experiment = {}
-# y_pred = clf.wisard(X_train, y_train, X_train, cfg.RAM_STD)
-# print(y_pred)
-
-# df_experiments['{}_test'.format(cfg.RAM_STD)] = y_train
-# df_experiments['{}_pred'.format(cfg.RAM_STD)] = y_pred
-# print(df_experiments['16_test'])
-# print(df_experiments['16_pred'])
-
+if __name__=="__main__":
+   for ram in range(cfg.MIN_RAM_SIZE, cfg.MAX_RAM_SIZE+1):
+      experiment = Experiment(ram)
+      experiment.start()
