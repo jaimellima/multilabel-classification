b50edd6e5c22c62e0809afd2150594a6b8482e85
Jaimel de Oliveira Lima <jaimeloliveiralima@gmail.com>
2022-07-20 11:54:12 -0300

    Padrão de Projeto

--- services/preprocessing/preprocess.py
+++ services/preprocessing/preprocess.py
@@ -4,33 +4,16 @@ from random import sample
 
 import pandas as pd
 import numpy as np
+from abc import ABC, abstractmethod
+from __future__ import annotations
 
 
 class Preprocess:
-    def __init__(self):
+    def __init__(self, vectorization_strategy: VectorizationStrategy):
         self._dataset = None
-        self._X = None
-        self._y = None
         self._stopwords = None
+        self._vectorization_strategy = vectorization_strategy
 
-    def set_dataset(self, origin_csv_file, n_sample):
-        self._dataset = pd.read_csv(origin_csv_file)
-        self._dataset = self._dataset.sample(n=n_sample)
-
-    def get_dataset(self):
-        return self._dataset
-
-    def set_X(self, x_columns):
-        self._X = self._dataset[x_columns].apply(lambda row: ''.join(row.values.astype(str)), axis=1)
-
-    def get_X(self):
-        return self._X
-
-    def get_y(self):
-        return self._y
-
-    def set_y(self, y_columns):
-        self._y = self._dataset[y_columns]
 
     def remove_punctuations(self, text):
         cleaned_text = text.lower()
@@ -38,13 +21,8 @@ class Preprocess:
         return cleaned_text
 
     def remove_stopwords(self, text):
-        cleaned_text = [word for word in text if not word in self._stopwords]
-        return cleaned_text
-
-    def get_preprocessed_text(self):
-        documents = np.array(self._X)
         X_preprocessed = []
-        for text in documents:
+        for text in self._dataset.get_documents():
             cleaned_text = self.remove_punctuations(text)
             cleaned_text = self.remove_stopwords(cleaned_text)
             cleaned_text = " ".join([word for word in cleaned_text])
@@ -53,11 +31,14 @@ class Preprocess:
             X_preprocessed.append(cleaned_text)
         return X_preprocessed
 
-    def vectorization(self):
-        #strategy??? abertas para extensão, mas fechadas para modificação
-        pass
+    def vectorization(self, text, vectorization_strategy):
+        return self._vectorization_strategy.vectorize(text)
+        
         
     
+class VectorizationStrategy():
+    @abstractmethod
+    def vectorize(self, text): pass
 
 
         
